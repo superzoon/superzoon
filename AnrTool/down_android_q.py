22ae79a4921db900e665a3c05838cc8c476c3dad
xlan <303106251@qq.com>
2019-11-08 09:50:07 +0800

    下载代码更新

--- AnrTool/down_android_q.py
+++ AnrTool/down_android_q.py
@@ -2,12 +2,26 @@ import xml.dom.minidom
 import os
 from subprocess import call
 from os import (popen,makedirs, chdir, symlink)
-from os.path import (isdir, isfile, sep, dirname)
-from shutil import rmtree
-def downloadAndroidSource(path='', name='',down_dir=sep.join(['d:','android_source']), manifest = False, msm = False):
+from os.path import (isdir, sep, dirname, exists)
+google_addr='https://android.googlesource.com/'
+tsinghua_addr='https://aosp.tuna.tsinghua.edu.cn/'
+download_addr=tsinghua_addr if True else google_addr
+
+android_manifest_git=download_addr+'platform/manifest.git'
+kernel_common_git=download_addr+'kernel/common.git'
+kernel_exynos_git=download_addr+'kernel/exynos.git'
+kernel_goldfish_git=download_addr+'kernel/goldfish.git'
+kernel_hikey_linaro_git=download_addr+'kernel/hikey-linaro.git'
+kernel_msm_git=download_addr+'kernel/msm.git'
+kernel_omap_git=download_addr+'kernel/omap.git'
+kernel_samsung_git=download_addr+'kernel/samsung.git'
+kernel_tegra_git=download_addr+'kernel/tegra.git'
+kernel_x86_64_git=download_addr+'kernel/x86_64.git'
+
+def downloadAndroidSource(path='', name='',down_dir=sep.join(['d:','android_source']), manifest = False, goldfish = False):
     if not isdir(down_dir):
         makedirs(down_dir)
-    msm_dir = sep.join([down_dir,'msm'])
+    goldfish_dir = sep.join([down_dir,'goldfish'])
     manifest_dir = sep.join([down_dir,'manifest'])
     android_source = sep.join([down_dir,'android_q'])
 
@@ -17,50 +31,58 @@ def downloadAndroidSource(path='', name='',down_dir=sep.join(['d:','android_sour
     if not os.path.exists(android_source):
         os.mkdir(android_source)
 
-    # prefix = "git clone https://android.googlesource.com/"
     # 3. 使用清华源下载地址
-    prefix ="git clone https://aosp.tuna.tsinghua.edu.cn/"
+    prefix ="git clone {}".format(download_addr)
     suffix = ".git"
 
     # 下载manifest函数
     def downloadManifest():
         print('down manifest')
-        chdir(down_dir)
-        # popen('git clone https://android.googlesource.com/platform/manifest.git')
-        call('git clone https://aosp.tuna.tsinghua.edu.cn/platform/manifest.git')
-        chdir(manifest_dir)
-        call('git checkout android-q-preview-2.5')
+        if not isdir(manifest_dir):
+            chdir(down_dir)
+            call('git clone {}'.format(android_manifest_git))
+            chdir(manifest_dir)
+            call('git checkout android-q-preview-2.5')
+        else:
+            chdir(manifest_dir)
+            call('git pull')
+
 
     # 下载kernel函数
-    def downloadMsm():
-        print('down msm')
-        chdir(down_dir)
-        # popen('git clone https://android.googlesource.com/kernel/msm.git')
-        call('git clone https://aosp.tuna.tsinghua.edu.cn/kernel/msm.git')
-        chdir(msm_dir)
-        call('git checkout android-q-preview-2.5')
+    def downloadGoldfish():
+        print('down goldfish')
+        if not isdir(goldfish_dir):
+            chdir(down_dir)
+            call('git clone {}'.format(kernel_goldfish_git))
+            chdir(goldfish_dir)
+            call('git checkout android-q-preview-2.5')
+        else:
+            chdir(goldfish_dir)
+            call('git pull')
+
 
     # 下载某一个模块函数
     def downloadMode(rootdir, path, name):
-        os.chdir(rootdir)
-        last = path.rfind("/")
-        if last != -1:
-            d = rootdir + "/" + path[:last]
-            if not os.path.exists(d):
-                os.makedirs(d)
-            os.chdir(d)
-        call(prefix + name + suffix)
+        chdir(rootdir)
+        if not exists(path):
+            last = path.rfind("/")
+            if last != -1:
+                d = rootdir + "/" + path[:last]
+                if not exists(d):
+                    makedirs(d)
+                chdir(d)
+            call(prefix + name + suffix)
+        else:
+            chdir(rootdir+'/'+path)
+            call('git pull')
 
-     # 4. 下载msm
-    if msm or not isdir(msm_dir):
-        if isdir(msm_dir):
-            rmtree(msm_dir)
-        downloadMsm()
 
+     # 4. 下载goldfish
+    if goldfish:
+        downloadGoldfish()
+    exit(0)
      # 5. 下载manifest
-    if manifest:
-        if isdir(manifest_dir):
-            rmtree(manifest_dir)
+    if manifest or not isdir(manifest_dir):
         downloadManifest()
 
 
@@ -77,10 +99,10 @@ def downloadAndroidSource(path='', name='',down_dir=sep.join(['d:','android_sour
             name = node.getAttribute("name")
             if path and name:
                 downloadMode(rootdir, node.getAttribute("path"), node.getAttribute("name"))
-    #创建软连接msm -> android_q/kernel/msm
-    if isdir(msm_dir):
-        src = msm_dir
-        dst = sep.join([android_source,'kernel','msm'])
+    #创建软连接goldfish -> android_q/kernel/goldfish
+    if isdir(goldfish_dir):
+        src = goldfish_dir
+        dst = sep.join([android_source,'kernel','goldfish'])
         if not isdir(dirname(dst)):
             makedirs(dirname(dst))
         symlink(src, dst)
@@ -89,6 +111,6 @@ if __name__ == '__main__':
     if email == '303106251@qq.com':
         path = 'frameworks/base'
         name = 'platform/frameworks/base'
-        downloadAndroidSource(path=path, name=name, msm=True)
+        downloadAndroidSource(path=path, name=name, goldfish=True)
     else:
         downloadAndroidSource()
